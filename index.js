614ff132190a2927c01d7b17ee9163dd50881b61
LeandroHernandez <johanhernandezvelez@gmail.com>
2024-04-10 21:44:36 -0500

    Todo listo

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const sections = document.querySelectorAll("section[id]");
   const navLinks = document.querySelectorAll("nav a");
 
+  // Función para verificar si una sección está en el viewport
   function isSectionInViewport(section) {
     const rect = section.getBoundingClientRect();
     const viewportHeight =
@@ -17,6 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
     );
   }
 
+  // Función para agregar la clase 'active' al enlace correspondiente cuando una sección está en el viewport
   function toggleActiveClass() {
     sections.forEach((section) => {
       const sectionId = section.getAttribute("id");
@@ -32,45 +34,10 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
+  // Evento de desplazamiento para actualizar el estado de los enlaces al desplazarse por la página
   window.addEventListener("scroll", toggleActiveClass);
   window.addEventListener("resize", toggleActiveClass);
 
+  // Llamar a la función inicialmente para aplicar los estilos correctos al cargar la página
   toggleActiveClass();
-
-  // Agregar un controlador de eventos para el envío del formulario
-  const contactForm = document.getElementById("contactForm");
-  contactForm.addEventListener("submit", function (event) {
-    event.preventDefault(); // Evitar el envío por defecto del formulario
-
-    // Obtener los valores de los campos del formulario
-    const name = document.querySelector(".name").value;
-    const phone = document.querySelector(".phone").value;
-    const email = document.querySelector(".email").value;
-    const subject = document.querySelector(".subject").value;
-    const message = document.querySelector(".message").value;
-
-    // Realizar la solicitud AJAX para enviar los datos
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", "tu_url_de_envio_aqui"); // Reemplaza 'tu_url_de_envio_aqui' con la URL de tu servidor para enviar mensajes
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onload = function () {
-      if (xhr.status === 200) {
-        alert("Mensaje enviado con éxito");
-        // Puedes agregar aquí cualquier otra acción que desees realizar después de enviar el mensaje
-      } else {
-        alert(
-          "Hubo un problema al enviar el mensaje. Por favor, inténtalo de nuevo más tarde."
-        );
-      }
-    };
-    xhr.send(
-      JSON.stringify({
-        name: name,
-        phone: phone,
-        email: email,
-        subject: subject,
-        message: message,
-      })
-    );
-  });
 });
